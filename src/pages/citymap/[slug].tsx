fb65b28ac51b3121620ce94e82bf865296e124bc
Frank Gimeno <frankgimeno3@gmail.com>
2023-05-03 17:48:44 +0200

    added slugs to city places and people

---
+++ src/pages/citymap/[slug].tsx
@@ -0,0 +1,7 @@
+//pàgina que es mostra quan cliquem a un botó en relació d'una àrea
+
+// Mostrarà:
+// Nom de l'àrea
+// Descripció del àrea
+// Qui hi ha i què hi fa
+// Altres àrees relacionades
---
+++ src/pages/profiles/[slug].tsx
@@ -0,0 +1,17 @@
+//pàgina que es mostraquan cliquem a un perfil, que dependrà del slug que li passem
+
+// Mostrarà:
+// Nom i cognoms
+// edat i sexe
+// viu o mort
+// Casat o solter (si és casat, amb qui)
+// lloc de treball
+// professió actual
+// horari de treball
+// número de fills
+// risc d'enfermetat
+// probabilitat de tenir un altre fill
+// probabilitat de morir espontàniament
+// religiositat
+
+//on és ara mateix
